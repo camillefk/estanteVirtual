73d97f7b65b9f9bd1b76fa4d3492f7a27d70ccae
Camille Focke <contatocamillefarias@gmail.com>
2025-01-23 21:03:54 -0300

    Background, box center e adição da fonte Josefin Sans

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="box-inicial">
+      <div className="center-box">
+        <h2>Sejam bem-vindos a minha Estante Virtual!</h2>
+        <p>Este é o lugar onde eu registro todos os livros que eu já li, onde dou minhas avaliações e minhas opniões acerca de cada livro. Um lugar mágico onde guardo memórias felizes e acolhedoras. </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
