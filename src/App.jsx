c18cbd3d73322254cc3298e8d2d2193a88d335ab
Camille Focke <contatocamillefarias@gmail.com>
2025-01-26 15:12:46 -0300

    personalização do botão e alinhamento dos elementos

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="box-inicial">
+      <h1 className="title-inicial">ESTANTE VIRTUAL</h1>
       <div className="center-box">
-        <h2>Sejam bem-vindos a minha Estante Virtual!</h2>
-        <p>Este é o lugar onde eu registro todos os livros que eu já li, onde dou minhas avaliações e minhas opniões acerca de cada livro. Um lugar mágico onde guardo memórias felizes e acolhedoras. </p>
+        <h2>SEJAM BEM-VINDOS A MINHA ESTANTE VIRTUAL</h2>
+        <p>
+          Este é o lugar onde eu registro todos os livros que eu já li, onde dou
+          minhas avaliações e minhas opiniões acerca de cada livro. Um lugar
+          mágico onde guardo memórias felizes e acolhedoras.
+        </p>
+        <button className="button-inicial">ENTRAR</button>
       </div>
     </div>
   );
